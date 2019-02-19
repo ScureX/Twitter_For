658f30cd1c79d8e388fc995cbc048975ca393e57
ScureX <47725553+ScureX@users.noreply.github.com>
2019-02-19 19:53:21 +0100

    Add files via upload

--- Twitter for.py
+++ Twitter for.py
@@ -9,9 +9,20 @@ api = tweepy.API(auth)
 
 # # TWEET # #
 
+# inputs #
+
 tweet = input("Tweet: ")
-reply = input("Reply:")
-user = input("User:")
+reply = input("Reply: ")
+user = input("User: ")
+
+# this is so u dont have to put the @<Name> in ur tweet if u wanna reply to someone #
+
+if user == "":
+    tweetF = tweet
+else:
+    tweetF= "@"+(user)+"  "+(tweet)
+
+# some kind of checking if the tweet id is correct #
 
 length = 28 + len(user)
 print(length)
@@ -19,5 +30,8 @@ print(length)
 replyF = reply[length:]
 print(replyF)
 
-api.update_status(status =(tweet), in_reply_to_status_id =(replyF))
+# sending the tweet #
+
+api.update_status(status =(tweetF), in_reply_to_status_id =(replyF))
 print ("-------successfully sent-------")
+
