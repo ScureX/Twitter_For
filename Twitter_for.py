8173da74a05a6f50b8288f5e9d25f42a25abb729
ScureX <47725553+ScureX@users.noreply.github.com>
2019-02-20 16:56:57 +0100

    Add files via upload

--- Twitter_for.py
+++ Twitter_for.py
@@ -9,28 +9,67 @@ api = tweepy.API(auth)
 
 # # TWEET # #
 
-# inputs #
+cred = (" www.github.com/scurex")
 
-tweet = input("Tweet: ")
-reply = input("Reply: ")
-user = input("User: ")
+
+loop = True
+while loop:
+
+# inputs #
+ print('www.github.com/scurex')
+ 
+ tweet = input("Tweet: ")
+ reply = input("Reply: ")
+ user = input("User: ")
 
 # this is so u dont have to put the @<Name> in ur tweet if u wanna reply to someone #
 
-if user == "":
+ if user == "":
     tweetF = tweet
-else:
+ else:
     tweetF= "@"+(user)+"  "+(tweet)
 
-# some kind of checking if the tweet id is correct #
+# some kind of error checking #
 
-length = 28 + len(user)
-print(length)
+ length = 28 + len(user)
+ print(length)
 
-replyF = reply[length:]
-print(replyF)
+ replyF = reply[length:]
+ print(replyF)
 
+
+# checks if tweet is too long and if yes how much characters u have to delete
+
+ if len(tweetF) + len(cred) > 280:
+    errLong = (len(tweetF) + len(cred)) - 280
+    
+    tweet = tweet[:errLong]
+    tweetF = ""
+    reply = ""
+    user = ""
+    
+    print("---error! your tweet is " + str(errLong) + " characters too long---")
+    print("--- TRY AGAIN ---")
+    
+    continue
+    
+ else:
 # sending the tweet #
+    api.update_status(status =(tweetF+(cred)), in_reply_to_status_id =(replyF))
 
-api.update_status(status =(tweetF), in_reply_to_status_id =(replyF))
-print("-------successfully sent-------")
+
+    print("-------successfully sent-------")
+    
+    retry = input("Send another Tweet? (y/n) ")
+    
+    if retry == ("y"):
+        continue
+    elif retry == ("n"):
+        print("okay byee")
+        break
+    else:
+        print(retry + (" is not an available option"))
+        break
+ 
+    
+    
